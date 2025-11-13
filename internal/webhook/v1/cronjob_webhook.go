7fe29d305eeb800310983e703db285d71843fcde
Vicen <xqfxqf111@gmail.com>
2025-11-13 09:00:01 +0800

    Implementing defaulting/validating webhooks

--- internal/webhook/v1/cronjob_webhook.go
+++ internal/webhook/v1/cronjob_webhook.go
@@ -20,6 +20,12 @@ import (
 	"context"
 	"fmt"
 
+    "github.com/robfig/cron"
+    apierrors "k8s.io/apimachinery/pkg/api/errors"
+    "k8s.io/apimachinery/pkg/runtime/schema"
+    validationutils "k8s.io/apimachinery/pkg/util/validation"
+    "k8s.io/apimachinery/pkg/util/validation/field"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -37,11 +43,14 @@ var cronjoblog = logf.Log.WithName("cronjob-resource")
 func SetupCronJobWebhookWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewWebhookManagedBy(mgr).For(&batchv1.CronJob{}).
 		WithValidator(&CronJobCustomValidator{}).
-		WithDefaulter(&CronJobCustomDefaulter{}).
+		WithDefaulter(&CronJobCustomDefaulter{
+			DefaultConcurrencyPolicy:		  batchv1.AllowConcurrent,
+			DefaultSuspend:					  false,
+			DefaultSuccessfulJobsHistoryLimit: 3,
+			DefaultFailedJobsHistoryLimit:	  1,
+		}).
 		Complete()
 }
-
-// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 
 // +kubebuilder:webhook:path=/mutate-batch-tutorial-kubebuilder-io-v1-cronjob,mutating=true,failurePolicy=fail,sideEffects=None,groups=batch.tutorial.kubebuilder.io,resources=cronjobs,verbs=create;update,versions=v1,name=mcronjob-v1.kb.io,admissionReviewVersions=v1
 
@@ -51,7 +60,11 @@ func SetupCronJobWebhookWithManager(mgr ctrl.Manager) error {
 // NOTE: The +kubebuilder:object:generate=false marker prevents controller-gen from generating DeepCopy methods,
 // as it is used only for temporary operations and does not need to be deeply copied.
 type CronJobCustomDefaulter struct {
-	// TODO(user): Add more fields as needed for defaulting
+	// Default values for various CronJob fields
+	DefaultConcurrencyPolicy		  batchv1.ConcurrencyPolicy
+	DefaultSuspend					  bool
+	DefaultSuccessfulJobsHistoryLimit int32
+	DefaultFailedJobsHistoryLimit	  int32
 }
 
 var _ webhook.CustomDefaulter = &CronJobCustomDefaulter{}
@@ -65,12 +78,31 @@ func (d *CronJobCustomDefaulter) Default(_ context.Context, obj runtime.Object) 
 	}
 	cronjoblog.Info("Defaulting for CronJob", "name", cronjob.GetName())
 
-	// TODO(user): fill in your defaulting logic.
+	// Set default values
+	d.applyDefaults(cronjob)
 
 	return nil
 }
 
-// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
+func (d * CronJobCustomDefaulter) applyDefaults (cronJob * batchv1.CronJob)  {
+	if cronJob.Spec.ConcurrencyPolicy == "" {
+		cronJob.Spec.ConcurrencyPolicy = d.DefaultConcurrencyPolicy
+	}
+	if cronJob.Spec.Suspend == nil {
+		cronJob.Spec.Suspend = new(bool)
+		*cronJob.Spec.Suspend = d.DefaultSuspend
+	}
+	if cronJob.Spec.SuccessfulJobsHistoryLimit == nil {
+		cronJob.Spec.SuccessfulJobsHistoryLimit = new(int32)
+		*cronJob.Spec.SuccessfulJobsHistoryLimit = d.DefaultSuccessfulJobsHistoryLimit
+	}
+	if cronJob.Spec.FailedJobsHistoryLimit == nil {
+		cronJob.Spec.FailedJobsHistoryLimit = new(int32)
+		*cronJob.Spec.FailedJobsHistoryLimit = d.DefaultFailedJobsHistoryLimit
+	}
+
+}
+
 // NOTE: The 'path' attribute must follow a specific pattern and should not be modified directly here.
 // Modifying the path for an invalid path can cause API server errors; failing to locate the webhook.
 // +kubebuilder:webhook:path=/validate-batch-tutorial-kubebuilder-io-v1-cronjob,mutating=false,failurePolicy=fail,sideEffects=None,groups=batch.tutorial.kubebuilder.io,resources=cronjobs,verbs=create;update,versions=v1,name=vcronjob-v1.kb.io,admissionReviewVersions=v1
@@ -94,9 +126,7 @@ func (v *CronJobCustomValidator) ValidateCreate(_ context.Context, obj runtime.O
 	}
 	cronjoblog.Info("Validation for CronJob upon creation", "name", cronjob.GetName())
 
-	// TODO(user): fill in your validation logic upon object creation.
-
-	return nil, nil
+	return nil, validateCronJob(cronjob)
 }
 
 // ValidateUpdate implements webhook.CustomValidator so a webhook will be registered for the type CronJob.
@@ -107,9 +137,7 @@ func (v *CronJobCustomValidator) ValidateUpdate(_ context.Context, oldObj, newOb
 	}
 	cronjoblog.Info("Validation for CronJob upon update", "name", cronjob.GetName())
 
-	// TODO(user): fill in your validation logic upon object update.
-
-	return nil, nil
+	return nil, validateCronJob(cronjob)
 }
 
 // ValidateDelete implements webhook.CustomValidator so a webhook will be registered for the type CronJob.
@@ -123,4 +151,37 @@ func (v *CronJobCustomValidator) ValidateDelete(ctx context.Context, obj runtime
 	// TODO(user): fill in your validation logic upon object deletion.
 
 	return nil, nil
+}
+
+func validateCronJob(cronJob *batchv1.CronJob) error {
+	var allErrs field.ErrorList
+	if err := validateCronJobName(cronJob); err != nil {
+		allErrs = append(allErrs, err)
+	}
+	if err := validateCronJobSpec(cronJob); err != nil {
+		allErrs = append(allErrs, err)
+	}
+	if len(allErrs) == 0 {
+		return nil
+	}
+
+	return apierrors.NewInvalid(schema.GroupKind{Group: "batch.tutorial.kubebuilder.io", Kind: "CronJob"}, cronJob.Name, allErrs)
+}
+
+func validateCronJobSpec(cronJob *batchv1.CronJob) *field.Error {
+	return	validateScheduleFormat(cronJob.Spec.Schedule, field.NewPath("spec").Child("schedule"))
+}
+
+func validateScheduleFormat(schedule string, fldPath *field.Path) *field.Error {
+	if _, err := cron.ParseStandard(schedule); err != nil {
+		return field.Invalid(fldPath, schedule, err.Error())
+	}
+	return nil
+}
+
+func validateCronJobName(cronJob *batchv1.CronJob) *field.Error {
+	if len(cronJob.Name) > validationutils.DNS1035LabelMaxLength {
+		return field.Invalid(field.NewPath("metadata").Child("name"), cronJob.Name, "must be no more than 52 characters")
+	}
+	return nil
 }
